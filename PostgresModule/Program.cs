c84f1402d29304eebe9038299168e2a5651b9a7e
nktrtmv <nvartemov@edu.hse.ru>
2022-09-17 23:01:39 +0300

    added postgres module

--- PostgresModule/Program.cs
+++ PostgresModule/Program.cs
@@ -1,8 +1,57 @@
-using Microsoft.AspNetCore.Builder;
+using Docker.DotNet;
+using Docker.DotNet.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-app.MapGet("/", () => "Hello World!");
+WebApplication app = builder.Build();
+
+DockerClient client;
+
+
+app.MapGet("/create-postgres-container/{ip}", async (string ip) =>
+{
+    try
+    {
+        client = new DockerClientConfiguration(new Uri("http://" + ip + ":2375/")).CreateClient();
+
+        if (!await ImagesContains("postgres:latest"))
+        {
+            await client.Images.CreateImageAsync(
+                new ImagesCreateParameters
+                {
+                    FromImage = "postgres",
+                    Tag = "latest"
+                },
+                new AuthConfig
+                {
+                    Email = "test@example.com",
+                    Username = "test",
+                    Password = "password"
+                },
+                new Progress<JSONMessage>());
+        }
+
+        await client.Containers.CreateContainerAsync(new CreateContainerParameters
+        {
+            Image = "postgres",
+            HostConfig = new HostConfig
+            {
+                DNS = new[] {"127.0.0.1"}
+            }
+        });
+    }
+    catch
+    {
+        return Results.StatusCode(500);
+    }
+
+    return Results.Ok("Container built with ip: " + ip);
+});
+
+async Task<bool> ImagesContains(string repoTag)
+{
+    IList<ImagesListResponse> images = await client.Images.ListImagesAsync(new ImagesListParameters());
+    return images.Any(image => image.RepoTags.Contains(repoTag));
+}
 
 app.Run();
